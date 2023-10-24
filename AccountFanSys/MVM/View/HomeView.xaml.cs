06a8a8b7c36931b97b1326785ead378b8ebe99b5
202060230 <202060230@ucc.mx>
2023-10-24 13:13:35 -0600

    style: botón de utilidad funciona

--- AccountFanSys/MVM/View/HomeView.xaml.cs
+++ AccountFanSys/MVM/View/HomeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace AccountFanSys.MVM.View;
 
@@ -7,5 +8,25 @@ public partial class HomeView : UserControl
     public HomeView()
     {
         InitializeComponent();
+    }
+    
+    private void UtilidadButton_Click(object sender, RoutedEventArgs e)
+    {
+        UtilidadPopup.IsOpen = true;
+    }
+
+    private void GuardarUtilidad_Click(object sender, RoutedEventArgs e)
+    {
+        // Aquí puedes obtener el valor ingresado en el TextBox y realizar las acciones de guardado
+        string utilidad = UtilidadInput.Text;
+    
+        // Cierra la ventana emergente
+        UtilidadPopup.IsOpen = false;
+    }
+    
+    private void CancelarUtilidad_Click(object sender, RoutedEventArgs e)
+    {
+        // Cierra la ventana emergente sin guardar cambios
+        UtilidadPopup.IsOpen = false;
     }
 }
