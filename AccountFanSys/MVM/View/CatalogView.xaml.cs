f92b72daf507c10e01a8ead2adbc33962ceb3b8e
202060230 <202060230@ucc.mx>
2023-10-30 15:26:06 -0600

    function: agrega o quita inputs

--- AccountFanSys/MVM/View/CatalogView.xaml.cs
+++ AccountFanSys/MVM/View/CatalogView.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Windows.Controls;
-
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 namespace AccountFanSys.MVM.View;
 
 public partial class CatalogView : UserControl
@@ -7,5 +8,61 @@ public partial class CatalogView : UserControl
     public CatalogView()
     {
         InitializeComponent();
+        // Suscribe el método de manejo de eventos al evento de selección cambiada del ComboBox
+        categoryComboBox.SelectionChanged += CategoryComboBox_SelectionChanged;
     }
+    
+    private void CategoryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        // Obtiene el ComboBox seleccionado
+        ComboBox comboBox = (ComboBox)sender;
+
+        // Obtiene el índice seleccionado
+        int selectedIndex = comboBox.SelectedIndex;
+
+        // Configura la visibilidad de los campos de entrada según la selección
+        switch (selectedIndex)
+        {
+            case 0:
+                ShowInputs(8);
+                break;
+            case 1:
+                ShowInputs(7);
+                break;
+            // Agrega más casos según sea necesario para otras selecciones
+            default:
+                break;
+        }
+    }
+
+    private void ShowInputs(int count)
+    {
+        // Oculta todos los campos de entrada
+        HideAllInputs();
+
+        // Muestra la cantidad de campos de entrada especificada
+        for (int i = 1; i <= count; i++)
+        {
+            TextBox input = FindName($"input{i}") as TextBox;
+            if (input != null)
+            {
+                input.Visibility = Visibility.Visible;
+            }
+        }
+    }
+
+    private void HideAllInputs()
+    {
+        for (int i = 1; i <= 8; i++)
+        {
+            TextBox input = FindName($"input{i}") as TextBox;
+            if (input != null)
+            {
+                input.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+    
+    
+    
 }
